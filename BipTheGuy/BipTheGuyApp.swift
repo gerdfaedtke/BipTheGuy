37061ae3b666777ae64659a9a68ee1b9adea639e
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-21 17:46:32 +0100

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Gerd Faedtke on 21.01.24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
